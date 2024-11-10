5446cc9faa22ad7757de7be1f813c6044693e49f
cgbridgewater <cgbridgewater@outlook.com>
2024-11-09 22:32:18 -0800

    add errors above keyboard

--- client/src/components/WordleGame/WordleKey.jsx
+++ client/src/components/WordleGame/WordleKey.jsx
@@ -3,7 +3,7 @@ const keysRow1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'BACKSPACE']
 const keysRow2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER'];
 const keysRow3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
-const WordleKey = ({ onKeyPress, keyStatus }) => {
+const WordleKey = ({ onKeyPress, keyStatus, invalidWord, guessError }) => {
 
   // Function to get the key's color based on its status
   const getKeyClass = (key) => {
@@ -12,9 +12,12 @@ const WordleKey = ({ onKeyPress, keyStatus }) => {
     // Use feedback status as the class (correct, present, absent)
     return keyStatus[key];
   };
-
+  // &nbsp;
   return (
     <div className="wordle-keyboard">
+      {/* Error Statements */}
+      {guessError ? <p id="guess_error">Incomplete guess</p> : "" }
+      {invalidWord ? <p id="guess_error">Invalid Word</p> : "" }
       {/* First row */}
       <div className="wordle-key-row">
         {keysRow1.map((key, index) => (
