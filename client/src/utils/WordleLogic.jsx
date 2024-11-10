6d777ad30dda8b2b8d715b7899a1755bb7931b88
cgbridgewater <cgbridgewater@outlook.com>
2024-11-09 18:35:43 -0800

    replace word list, and use word list not axios.  add validate axios call

--- client/src/utils/WordleLogic.jsx
+++ client/src/utils/WordleLogic.jsx
@@ -1,43 +1,46 @@
 import axios from "axios"
+// Import JSON file
+import wordList from '../assets/Json/WordleList.json';
 
-// Pull a random word from the word from API source
-export const getRandomWord = async () => {
+
+// Check if the word is valid
+export const validateWord = async (word) => {
   try {
-    const res = await axios.get("https://random-word-api.herokuapp.com/word?length=5");
-    return res.data[0].toUpperCase(); // Return the first word from the response in caps
+    const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    // Word is valid
+    return true;
   } catch (error) {
-    console.error("Error fetching random word:", error);
-    return null; // Return null in case of error
+    // Return false if the word is not found
+    return false;
   }
 };
 
-//                                                    //
-// DO NOT DELETE WE NEED THIS FOR VALIDATION OF WORDS //
-//                                                    //
-
-// Check if the word is valid
-// export const validateWord = (word) => {
-//   console.log("WORD: ", word)
-//   return wordList.words.includes(word.toLowerCase());
-// };
-
-//                                                    //
-// DO NOT DELETE WE NEED THIS FOR VALIDATION OF WORDS //
-//                                                    //
-
-
-
-//                                                               //
-// DO NOT DELETE CAN BE USED FOR OUR OWN WORDS LIST IN JSON FILE //
-//                                                               //
-
-// Import JSON file
-// import wordList from '../assets/Json/WordList.json';
-
 // Select a random word from the word list
-// export const getRandomWord = () => {
-//   const words = wordList.words; // Access the 'words' array from the JSON
-//   const randomWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
-//   console.log("THE RANDOM WORD- ", randomWord);
-//   return randomWord;
-// };
+export const getRandomWord = () => {
+  const words = wordList.words; // Access the 'words' array from the JSON
+  const randomWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+  return randomWord;
+};
+
+
+
+
+
+
+
+//                                                                //
+// DO NOT DELETE CAN BE USED FOR FETCHING A WORD LIST FROM AN API (AXIOS NPM REQUIRED!!) //
+//                                                                //
+
+// import axios from "axios"
+
+  // Pull a random word from the word from API source
+  // export const getRandomWord = async () => {
+  //   try {
+  //     const res = await axios.get("https://random-word-api.herokuapp.com/word?length=5");
+  //     return res.data[0].toUpperCase(); // Return the first word from the response in caps
+  //   } catch (error) {
+  //     console.error("Error fetching random word:", error);
+  //     return null; // Return null in case of error
+  //   }
+  // };
