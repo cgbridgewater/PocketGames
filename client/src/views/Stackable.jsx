ce2235177d4920167ca844ebcf29f79090c538a3
cgbridgewater <cgbridgewater@outlook.com>
2024-11-11 21:44:00 -0800

    clean out component

--- client/src/views/Stackable.jsx
+++ client/src/views/Stackable.jsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom"
-import GameIcon from "../assets/images/GameIcon.png"
 
-function Stackable({ isWinningModalOpen, setIsWinningModalOpen }) {
+const Stackable = () => {
 
     return (
         <main>
-            <h1>This is a TEST</h1>
-
+            <h1>test</h1>
         </main>
-    );
+    )
+
 }
 
 export default Stackable;
