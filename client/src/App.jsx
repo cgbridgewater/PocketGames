2f5865096177fa60ef08d7ce6402fdf10c5b87cf
cgbridgewater <cgbridgewater@outlook.com>
2024-11-11 21:44:31 -0800

    remove stackable, add stackable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import Navbar from './components/NavBarComponent/Navbar'
+import Index from './views/Index'
 import GameMenu from './views/GamesMenu'
+import Memory from './views/Memory'
 import SimonSays from './views/SimonSays'
 import Wordle from './views/Wordle'
-import Memory from './views/Memory'
 import Stackable from './views/Stackable'
-import Index from './views/Index'
 import BadURL from './views/BadURL'
+import LightsOut from './views/LightsOut'
 
 function App() {
 
@@ -64,6 +65,22 @@ function App() {
             setIsWinningModalOpen={setIsWinningModalOpen}
           />}
         />
+
+        {/* Lights Out GAME */}
+        <Route path='/games/lightsout' element={
+          <LightsOut
+            isWinningModalOpen={isWinningModalOpen} 
+            setIsWinningModalOpen={setIsWinningModalOpen}
+          />}
+        />
+
+        {/* Stackable GAME */}
+        {/* <Route path='/games/stackable' element={
+          <Stackable
+            isWinningModalOpen={isWinningModalOpen} 
+            setIsWinningModalOpen={setIsWinningModalOpen}
+          />}
+        /> */}
 
 
         <Route path="*" element={
