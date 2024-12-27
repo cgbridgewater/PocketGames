dd58ddae93af461778321d57b15b609a6fb57623
cgbridgewater <cgbridgewater@outlook.com>
2024-12-26 21:12:13 -0800

    add state to control drawer

--- client/src/components/NavBarComponent/NavDrawer.jsx
+++ client/src/components/NavBarComponent/NavDrawer.jsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import RedLink from "./RedLink";
-// Import game JSON
 import GamesData from "../../assets/Json/GameList.json";
 
-export default function NavDrawer({ currentPath }) {
-
-    // Filter JSON for isActive = true and sort alphabetically
+export default function NavDrawer({ currentPath, handleNavClick }) {
+    // Filter JSON for active games and sort alphabetically
     const GameData = GamesData
         .filter(data => data.isActive && `/games/${data.url}` !== currentPath)
         .sort((a, b) => a.title.localeCompare(b.title));
@@ -12,21 +11,25 @@ export default function NavDrawer({ currentPath }) {
     return (
         <nav id="drawer">
             <ul>
-                {/* HOME LINK */}
-                <li className="drawer_link">
-                    <RedLink href="/" text="Home" external={false} />
-                </li>
-                {/* Mapping through game links from JSON file */}
-                { GameData
-                    .map((Game) => {
-                        return (
-                            <li className="drawer_link" key={Game.id}>
-                                <RedLink href={`/games/${Game.url}`} text={Game.title} external={Game.external_link} />
-                            </li>
-                        )
-                    })
+                {/* HOME LINK remove link if on games menu page */}
+                { currentPath === "/games" ?
+                    null
+                        :
+                    <li className="drawer_link" onClick={handleNavClick}>
+                        <RedLink href="/games" text="Games Menu" external={false} />
+                    </li>
                 }
+                {/* Game links */}
+                {GameData.map((game) => (
+                    <li className="drawer_link" key={game.id} onClick={handleNavClick}>
+                        <RedLink
+                            href={`/games/${game.url}`}
+                            text={game.title}
+                            external={game.external_link}
+                        />
+                    </li>
+                ))}
             </ul>
         </nav>
     );
-};
+}
--- client/src/components/NavBarComponent/Navbar.jsx
+++ client/src/components/NavBarComponent/Navbar.jsx
@@ -1,34 +1,38 @@
-import { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import PocketGamesIcon from "../../assets/images/PocketGames.png";
 import NavDrawer from './NavDrawer';
 
-export default function Navbar({ currentPath  }) {
-    // Handle clicks to close nav bar, either link or outside of drawer area
-    useEffect(() => {
-        const drawerLinks = document.querySelectorAll('.drawer_link');
-        drawerLinks.forEach(link => { 
-            link.addEventListener('click', () => { 
-                document.getElementById('drawer_toggle').checked = false;
-            });
-        });
-    }, []);
+export default function Navbar({ currentPath }) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleNavClick = () => {
+        setIsChecked(prev => !prev);
+    };
 
     return (
         <>
             {/* HAMBURGER CHECKBOX */}
-            <input type="checkbox" id="drawer_toggle" name="drawer_toggle"/>
-            {/* HAMBURGER */}
-            <label htmlFor="drawer_toggle" id="drawer_toggle_label"><span className="visually_hidden">Empty Link</span></label>
+            <input
+                type="checkbox"
+                id="drawer_toggle"
+                name="drawer_toggle"
+                checked={isChecked}
+                onChange={handleNavClick}
+            />
+            {/* HAMBURGER LABEL */}
+            <label  htmlFor="drawer_toggle" id="drawer_toggle_label" >
+                <span className="visually_hidden">Toggle Drawer</span>
+            </label>
             {/* NAV HEADER */}
             <header>
                 <Link to="/games">
-                                    <img className="logo" src={ PocketGamesIcon } alt="Pocket Games"/>
+                    <img className="logo" src={PocketGamesIcon} alt="Pocket Games" />
                 </Link>
             </header>
-            {/* END HEADER */}
+
             {/* SLIDE OUT DRAWER FOR HAMBURGER */}
-            <NavDrawer currentPath={ currentPath }/>
+            <NavDrawer currentPath={currentPath} handleNavClick={handleNavClick} />
         </>
     );
-};
+}
