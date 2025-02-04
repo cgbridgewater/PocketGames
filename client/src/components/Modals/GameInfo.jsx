aca0dcc2eaaef655282aabb3066f841bcbfb3802
cgbridgewater <cgbridgewater@outlook.com>
2025-02-04 13:07:40 -0800

    add function to add line split recognition

--- client/src/components/Modals/GameInfo.jsx
+++ client/src/components/Modals/GameInfo.jsx
@@ -1,30 +1,36 @@
 import { useState } from "react";
 
 const GameInfo = ({ howTo }) => {
-
-    // State
     const [isInfoOpen, setIsInfoOpen] = useState(false);
+    
+    // Split lines and render them with <br />
+    const formattedHowTo = howTo.split('\n').map((line, index) => (
+        <span key={index}>
+            {line}
+            {index < howTo.split('\n').length - 1 && <br />}
+        </span>
+    ));
 
     return (
         <>
             {
                 isInfoOpen ? (
-                    <div  className="modal_overlay">
+                    <div className="modal_overlay">
                         <div className="game_info">
                             <h3>How To Play</h3>
                             <p>
-                                {howTo}
+                                {formattedHowTo}
                             </p>
                             <div className="button_box">
-                                <button onClick={() => setIsInfoOpen(false)} >
-                                Close
+                                <button onClick={() => setIsInfoOpen(false)}>
+                                    Close
                                 </button>
                             </div>
                         </div>
-                    </ div>
+                    </div>
                 ) : null
             }
-            <button onClick={() => setIsInfoOpen(true)} title="Info" >
+            <button onClick={() => setIsInfoOpen(true)} title="Info">
                 Info
             </button>
         </>
