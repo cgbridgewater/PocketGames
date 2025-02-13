1bff1d91ed82468a7a7c0bf496cdad9c643df9b7
cgbridgewater <cgbridgewater@outlook.com>
2025-02-12 23:18:17 -0800

    remove onClick for overlay to close modal, forcing modal close button

--- client/src/components/Modals/WinningModal.jsx
+++ client/src/components/Modals/WinningModal.jsx
@@ -2,15 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function WinningModal({ message1, message2, turns, onClose }) {
 
-    // Closes Modal when overlay is clicked  <-- Maybe delete this to force a button push??
-    const handleOverlayClick = (e) => {
-        if (e.target.className === 'modal_overlay') {
-            onClose();
-        }
-    };
-
     return (
-        <div className="modal_overlay" onClick={handleOverlayClick} >
+        <div className="modal_overlay">
             <div id="winning_modal">
                 <h2>Game Over!</h2>
                 <p>{message1} <span>{turns}</span> {message2}</p>
